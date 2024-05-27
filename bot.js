1b85855b366a98115818b86784d8e71f81ea6e5b
Alim <alimalim77@gmail.com>
2024-05-27 12:22:53 +0530

    Role Branch

--- bot.js
+++ bot.js
@@ -25,32 +25,33 @@ const startBot = async () => {
       });
     });
 
-    client.on("messageCreate", async (msg) => {
-      if (msg.author.bot) return;
-      console.log(msg.guild.members);
-      if (verifiedUsers.has(msg.author.id)) {
-        msg.channel.send("You are already verified.");
-        return;
-      }
+    // client.on("messageCreate", async (msg) => {
+    //   if (msg.author.bot) return;
+    //   console.log(msg.guild.members);
+    //   if (verifiedUsers.has(msg.author.id)) {
+    //     msg.channel.send("You are already verified.");
+    //     return;
+    //   }
 
-      const userEmail = data.find((entry) => entry.Email === msg.content);
-      if (userEmail) {
-        msg.channel.send("User is Verified ✔️😀");
-        verifiedUsers.add(msg.author.id);
-      } else {
-        if (msg.content.includes("@")) {
-          msg.channel.send("You are not verified in server😞");
-        }
-        try {
-          await msg.guild.members.ban(msg.author.id, {
-            reason: "Unverified email provided",
-          });
-          console.log(`Banned ${msg.author.tag} for unverified email`);
-        } catch (error) {
-          console.error(`Failed to ban ${msg.author.tag}:`, error);
-        }
-      }
-    });
+    //   const userEmail = data.find((entry) => entry.Email === msg.content);
+    //   if (userEmail) {
+    //     msg.channel.send("User is Verified ✔️😀");
+    //     verifiedUsers.add(msg.author.id);
+    //   } else {
+    //     if (msg.content.includes("@")) {
+    //       msg.channel.send("You are not verified in server😞");
+    //     }
+    //     try {
+    //       await msg.guild.members.ban(msg.author.id, {
+    //         reason: "Unverified email provided",
+    //       });
+    //       console.log(`Banned ${msg.author.tag} for unverified email`);
+    //     } catch (error) {
+    //       console.error(`Failed to ban ${msg.author.tag}:`, error);
+    //     }
+    //   }
+    // });
+
     client.login(process.env.TOKEN);
   } catch (err) {
     console.error("Error occurred:", err);
